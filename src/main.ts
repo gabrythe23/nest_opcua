089d39db3fcf661cf8476c67149b848bf164f77c
Gabriele Partiti <gabriele@leonardotravel.it>
2022-01-15 17:08:15 +0100

    convert app from http only to microservice and http

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+import {OpcUaSubscriber} from "./trasporter/opcua.transporter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+// Then combine it with your microservice
+    const microservice = app.connectMicroservice({
+        strategy: new OpcUaSubscriber(),
+    });
+
+    await app.startAllMicroservices();
+    await app.listen(3000);
 }
+
 bootstrap();
