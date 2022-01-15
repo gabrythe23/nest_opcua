992066b9c5b7483fb714903423882067ba01b988
Gabriele Partiti <gabriele@leonardotravel.it>
2022-01-15 18:44:39 +0100

    create socket gateway

---
+++ src/app.gateway.ts
@@ -0,0 +1,34 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
+
+@WebSocketGateway({cors:true, allowEIO3:true})
+export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() server: Server;
+  private logger: Logger = new Logger('AppGateway');
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(payload: object): void {
+    this.server.emit('msgToClient', payload);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+}
