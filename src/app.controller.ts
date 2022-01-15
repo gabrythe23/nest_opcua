e0f3e20f5171fb4e84dcea65b7dc3b311a78e907
Gabriele <gabrielepartiti@icloud.com>
2022-01-15 18:46:04 +0100

    Feature/send update to socket (#4)
    * add socket.io to dependencies
    
    * create socket gateway
    
    * send message to socket when new data is received
    
    * add simple frontend for display data
    
    * add static render for frontend client
    
    Co-authored-by: Gabriele Partiti <gabriele@leonardotravel.it>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import {MessagePattern, Payload} from "@nestjs/microservices";
+import {AppGateway} from "./app.gateway";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+      private readonly appService: AppService,
+      private readonly socketGateway: AppGateway,
+  ) {}
 
   @MessagePattern("ns=3;i=1004")
   async readOPCData(@Payload('value') value: number) {
-    await this.appService.saveData(value, "ns=3;i=1004");
+    const node : string = "ns=3;i=1004";
+    await this.appService.saveData(value, node);
+    this.socketGateway.handleMessage({name:node, text:value})
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ require('dotenv').config({ path: `./.env` });
 import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
 import {OpcUaSubscriber} from "./trasporter/opcua.transporter";
+import { join } from 'path';
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-
-// Then combine it with your microservice
+    const app = await NestFactory.create<NestExpressApplication>(AppModule,{ cors: true });
+    app.useStaticAssets(join(__dirname, '..', 'static'));
+    // Then combine it with your microservice
     const microservice = app.connectMicroservice({
         strategy: new OpcUaSubscriber(),
     });
