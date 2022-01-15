43369225b9af7874b2cf336ea19d82d7a50bb244
Gabriele Partiti <gabriele@leonardotravel.it>
2022-01-15 17:16:26 +0100

    create method with decorators in controllerfor retrieve message from opc

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {MessagePattern, Payload} from "@nestjs/microservices";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern("ns=3;i=1004")
+  readOPCData(@Payload() data: object) {
+    return data;
   }
 }
